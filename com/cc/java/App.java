233c93e1fe94b8cb8c9c8fe30fd737b482bfa5fb
Peter <schmidt.multimedia@gmail.com>
2022-04-26 11:48:18 +0200

    Arrays II / for -Schleife

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,23 +7,39 @@ public class App {
     
     public static void main(String[] args) {
      
-        // Deklaration: int
-        int[] intArr = new int [4];
-        output("Arr: " + intArr);
-        output("Array-Länge: " + intArr.length);
-        output("1.Element: " + intArr[0]);
+        // // 1. Array I
+        // // Deklaration: int
+        // int[] intArr = new int [4];
+        // output("Arr: " + intArr);
+        // output("Array-Länge: " + intArr.length);
+        // output("1.Element: " + intArr[0]);
 
-        intArr[0] = 1;  // Datenzuweisung (Überschreiben der Anfangswerte)
-        intArr[1] = 2;
-        intArr[2] = 3;
-        intArr[3] = 4;
-        intArr[4] = 10;  // Index ausserhalb der Grenze!!!
+        // intArr[0] = 1;  // Datenzuweisung (Überschreiben der Anfangswerte)
+        // intArr[1] = 2;
+        // intArr[2] = 3;
+        // intArr[3] = 4;
+        // intArr[4] = 10;  // Index ausserhalb der Grenze!!!
 
-        output("1.Element: " + intArr[0]);
-        output("2.Element: " + intArr[1]);
-        output("Letztes Element: " + intArr[3]);
-        output("Letztes Element: " + intArr[intArr.length-1]);
-        output("Test: " + intArr[10]);
+        // output("1.Element: " + intArr[0]);
+        // output("2.Element: " + intArr[1]);
+        // output("Letztes Element: " + intArr[3]);
+        // output("Letztes Element: " + intArr[intArr.length-1]);
+        // output("Test: " + intArr[10]);
+
+        // 2. Array II
+         String[] strArr = {"Ich","bin","Atanas","der","Coole"};
+         
+        //  output("1.Element: " + strArr[0]);
+        //  output("2.Element: " + strArr[1]);
+        //  output("3.Element: " + strArr[2]);  // 3,4,5,6,7, ....
+
+        for (int i = 0; i < strArr.length; i++) 
+        {
+            output(strArr[i]); 
+            // output("Element "+ Integer.toString(i + 1) + ": " + strArr[i]);        
+        }
+
+        
         
     }
 
