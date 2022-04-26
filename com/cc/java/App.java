732349b273c6c5f4f5ce4227bf973b35547d1992
Bernhard <b.vonwangenheim@gmail.com>
2022-04-26 10:38:35 +0200

    letze element intArr.length-1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,10 +6,21 @@ public class App {
      //   Import java.until.arrays;
      // Deklaration. int
       int[] intArr =  new int [4];
+      output("1.Element " + intArr[0]);
       output("arr: " + intArr);
-      output("1.Element" + intArr[0]);
       output("array-Länge" + intArr.length);
       
+intArr[0] = 1;
+
+intArr[1] = 2;
+intArr[2] = 3;
+intArr[3] = 4;
+output("1.Element: " + intArr[0]);
+output("2.Element: " + intArr[1]);
+output("3.Element: " + intArr[2]);
+output("letzeElement: " + intArr[3]);
+output("letze Element: " + intArr[intArr.length-1]);
+
 
     }
 
