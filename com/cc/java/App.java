76014268d98b6aeedfa027b16fdebd45c82eef1e
Bernhard <b.vonwangenheim@gmail.com>
2022-04-26 10:14:04 +0200

    jetzt läufts

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,14 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+     //   Import java.until.arrays;
+     // Deklaration. int
+      int[] intArr =  new int [4];
+      output("arr: " + intArr);
+      output("1.Element" + intArr[0]);
+      output("array-Länge" + intArr.length);
+      
+
     }
 
     public static void output(String outputStr) {
